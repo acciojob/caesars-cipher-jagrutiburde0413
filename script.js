2142d747f1aa687a5d2702abdeba1798bbc76bac
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-03-17 17:15:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 // Your Script here.
-
 var lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
+  A: "N",
+  B: "O",
+  C: "P",
+  D: "Q",
+  E: "R",
+  F: "S",
+  G: "T",
+  H: "U",
+  I: "V",
+  J: "W",
+  K: "X",
+  L: "Y",
+  M: "Z",
+  N: "A",
+  O: "B",
+  P: "C",
+  Q: "D",
+  R: "E",
+  S: "F",
+  T: "G",
+  U: "H",
+  V: "I",
+  W: "J",
+  X: "K",
+  Y: "L",
+  Z: "M",
+  "?": "?",
+  ",": ",",
 };
 
-function rot13(encodedStr){
-    var words = encodedStr.split(" "); // String to Array
+function rot13(encodedStr) {
+  var words = encodedStr.split(" "); // String to Array
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
   for (let i = 0; i < words.length; i++) {
@@ -24,7 +44,11 @@ function rot13(encodedStr){
     }
     decodedArr.push(decoded_word);
   }
-  return ;//return decodedArr
+  return decodedArr.join(" ");
 }
+
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
